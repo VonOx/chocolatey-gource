692b58d214fb3a418df192c5563d5de2185db6a6
Vincent KULAK <vkulak62@gmail.com>
2016-07-26 13:49:32 +0200

    Gource 0.42

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,41 @@
+﻿
+$ErrorActionPreference = 'Stop';
+
+
+$packageName= 'gource'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/acaudwell/Gource/releases/download/gource-0.42/gource-0.42-setup.exe'
+$url64      = ''
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  url64bit      = $url64
+
+  silentArgs    = "/S"
+  validExitCodes= @(0, 3010, 1641)
+
+  softwareName  = 'gource*'
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
