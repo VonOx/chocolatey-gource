ec8231a0cc91abc7e44feb6be3c7018c38769e7f
Vincent KULAK <vkulak62@gmail.com>
2016-08-10 15:15:31 +0200

    Adding checksum

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -5,14 +5,14 @@ $ErrorActionPreference = 'Stop';
 $packageName= 'gource'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'https://github.com/acaudwell/Gource/releases/download/gource-0.42/gource-0.42-setup.exe'
-$url64      = ''
 
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   fileType      = 'EXE'
   url           = $url
-  url64bit      = $url64
+  checksum      = 'FB81DDBB28BB3E65BE4CE0B76B6939B0D47093B485EB8AB86A88F05975C18BCD'
+  checksumType  = 'sha256'
 
   silentArgs    = "/S"
   validExitCodes= @(0, 3010, 1641)
