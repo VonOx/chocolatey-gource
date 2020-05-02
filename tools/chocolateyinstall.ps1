3d06b9d6eeb2081f71f680ec335d6052431f01f5
Vincent KULAK <vkulak62@gmail.com>
2020-05-02 23:20:35 +0200

    Gource V0.51

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -4,14 +4,14 @@ $ErrorActionPreference = 'Stop';
 
 $packageName= 'gource'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/acaudwell/Gource/releases/download/gource-0.47/gource-0.47.win64-setup.exe'
+$url        = 'https://github.com/acaudwell/Gource/releases/download/gource-0.51/gource-0.51.win64-setup.exe'
 
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   fileType      = 'EXE'
   url           = $url
-  checksum      = 'ca666e9c9fc9f69bb4ee52ed3914982575c917986ca984cd18b5edcab9016385'
+  checksum      = '853a53b76f9595d9fd8d0653cd838bc570cea5ea6deac5446a948a7b3000bea5'
   checksumType  = 'sha256'
 
   silentArgs    = "/S"
